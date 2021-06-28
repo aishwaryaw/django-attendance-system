2480b6a99feccf3622e1e71f03c62bf10066ce4f
aishwaryaw <aishwaryawalawalkar@gmail.com>
2021-06-29 00:53:27 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USERNAME'),
         'PASSWORD' :os.environ.get('DATABASE_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
